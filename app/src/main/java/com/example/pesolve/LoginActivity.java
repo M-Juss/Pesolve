cc12a6dbd0f95ba0062cd39df3e018c5fe3ab350
M-Juss <m.justinsayson@gmail.com>
2026-02-25 23:51:30 +0800

    Create Login page ui

--- app/src/main/java/com/example/pesolve/LoginActivity.java
+++ app/src/main/java/com/example/pesolve/LoginActivity.java
@@ -22,7 +22,6 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
