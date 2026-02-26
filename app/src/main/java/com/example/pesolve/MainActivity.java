b3e89c3a8c94ea2985fdc7ae9ab286268f3a3b67
M-Juss <m.justinsayson@gmail.com>
2026-02-26 15:14:08 +0800

    create functional fragments

--- app/src/main/java/com/example/pesolve/MainActivity.java
+++ app/src/main/java/com/example/pesolve/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.pesolve;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,18 +8,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.pesolve.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new TrasanctionFragment());
+
+        binding.analyticNav.setOnClickListener(v -> {
+            replaceFragment(new AnalyticsFragment());
+        });
+
+        binding.historyNav.setOnClickListener(v -> {
+            replaceFragment(new HistoryFragment());
+        });
+        binding.transactionNav.setOnClickListener(v -> {
+            replaceFragment(new TrasanctionFragment());
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
     }
 }
---
+++ app/src/main/java/com/example/pesolve/TrasanctionFragment.java
@@ -0,0 +1,22 @@
+package com.example.pesolve;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TrasanctionFragment extends Fragment {
+    View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_trasanction, container, false);
+        return view;
+    }
+}
